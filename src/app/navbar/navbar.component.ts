06fa3ba765190de46e0cb3c109a6d87d2d6e9853
Dominic Marra <dominicmarra321@gmail.com>
2019-09-25 00:23:40 -0700

    Added navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('openClose', [
+        state('open', style({
+            visibility: 'visible',
+            opacity: 1
+        })),
+        transition('open=>closed', animate('200ms ease-out')),
+        transition('closed=>open', animate('200ms ease-in'))
+    ])
+  ]
 })
+
 export class NavbarComponent implements OnInit {
+
+  private navState = 'closed'; //current state of the navbar when in mobile, used for animations
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  //Toggles between open and closed for nav menu
+  //toggles class for menu button for its animation
+  toggleMenuIcon() {
+    document.getElementById('menu').classList.toggle('activated');
+    this.navState === 'closed' ? this.navState = 'open' : this.navState = 'closed';
   }
 
 }
