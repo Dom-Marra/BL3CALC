b149a2a4e803180c759ee295a207afb846fb4bca
Dominic Marra <dominicmarra321@gmail.com>
2019-09-29 09:22:21 -0700

    Added route for Amara component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AmaraComponent } from './amara/amara.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'home', component: HomeComponent }
+  {path: 'home', component: HomeComponent },
+  {path: 'amara', component: AmaraComponent }
 ];
 
 @NgModule({
