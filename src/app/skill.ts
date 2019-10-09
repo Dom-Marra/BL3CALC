8ee0fbc738c2f3ac2cc3c876bf2b72aa9fd3afdc
Dominic Marra <dominicmarra321@gmail.com>
2019-10-09 09:37:54 -0700

    Now an abstract class with a few abstract methods and some pre-defined methods

--- src/app/skill.ts
+++ src/app/skill.ts
@@ -1,67 +1,26 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-
-export class Skill {
-    private readonly MIN_POINTS: number = 0;            //min number of points that can be allocated in the skill
-    private readonly NORMAL_SKILL_CASE: number = 0;     //Case if skill is normal
-    private readonly ACTION_SKILL_CASE: number = 1;     //Case if action skill
-    private readonly ACTION_MOD_CASE: number = 2;       //Case if action skill modifier
-    private readonly OTHER_CASE: number = 3;            //Case if other (pet, elemental type ...)
-
-    //Paths for box images for each type of skill
-    private readonly NORMAL_SKILL_BOX: string = "assets/images/skilltree/skillBox.png"; 
-    private readonly ACTION_SKILL_BOX: string = "assets/images/skilltree/actionSkillBox.png"; 
-    private readonly ACTION_SKILL_MOD_BOX: string = "assets/images/skilltree/actionSkillModBox.png"; 
-    private readonly OTHER_SKILL_BOX: string = "assets/images/skilltree/otherSkillBox.png"; 
+export abstract class Skill {
+    public readonly MIN_POINTS: number = 0;            //min number of points that can be allocated in the skill
     
     private allocatedPoints: number = 0;            //number of points allocated into the skill
     private path: string;                           //Image path
     private position: Array<number>;                //position on the tree [y, x]
     private maxPoints: number;                      //max number of points that can be allocated in the skill
     private preReq: number;                         //number of points required on the tree to allocate into this
-    private requiredActiveSkill: Skill = null;      //Required active skill to allocate active skill mod
-    private otherSkillType: number = 0;             //Defines if the skill is normal, action, action mod, or other
-    private skillBoxPath: string = "";              //The path for the box image behind the skill
+    private color: string;                          //The color group this skill belongs to 
 
-    
-    constructor(path: string, position: Array<number>, maxPoints: number, preReq: number, otherSkillType?: number, activeSkill?: Skill){
+
+    constructor(path: string, position: Array<number>, maxPoints: number, preReq: number, color: string) {
         this.path = path;                   
         this.position = position;            
         this.maxPoints = maxPoints;               
         this.preReq = preReq;
-
-
-        //Assign variables based on skill type
-        switch (otherSkillType) {
-            case this.ACTION_SKILL_CASE: {
-                this.otherSkillType = otherSkillType;
-                this.skillBoxPath = this.ACTION_SKILL_BOX;
-                break;
-            }
-            case this.ACTION_MOD_CASE: {
-                this.otherSkillType = otherSkillType;
-                this.requiredActiveSkill = activeSkill;
-                this.skillBoxPath = this.ACTION_SKILL_MOD_BOX;
-                break;
-            }
-            case this.OTHER_CASE: {
-                this.otherSkillType = otherSkillType;
-                this.requiredActiveSkill = activeSkill;
-                this.skillBoxPath = this.OTHER_SKILL_BOX;
-                break;
-            }
-            default: {
-                this.otherSkillType = this.NORMAL_SKILL_CASE;
-                this.skillBoxPath = this.NORMAL_SKILL_BOX;
-                break;
-            }
-        }
+        this.color = color;
     }
 
     /**
      * Adds a point into this skill
      */
     addPoint() {
-
         this.allocatedPoints++;
     }
 
@@ -73,6 +32,74 @@ export class Skill {
     }
 
     /**
+     * Returns the pre-req amount of points to allocate into this skill
+     * 
+     * @returns
+     *          number
+     */
+    getPreReq(): number {
+        return this.preReq;
+    }
+
+    /**
+     * Returns the allocated points into this skill
+     * 
+     * @returns
+     *          number
+     */
+    getAllocatedPoints(): number {
+        return this.allocatedPoints;
+    }
+
+    /**
+     * Returns the position of this skill on tree [y][x]
+     * 
+     * @returns
+     *          array
+     */
+    getPosition(): Array<number> {
+        return this.position;
+    }
+
+    /**
+     * Returns the path for the image
+     * 
+     * @returns
+     *          string
+     */
+    getPath(): string {
+        return this.path;
+    }
+
+    /**
+     * The number of maximum number of points that can be allocated into this skill
+     * @returns
+     *          number
+     */
+    getMaxPoints(): number {
+        return this.maxPoints;
+    }
+
+    /**
+     * Returns the color group this skill belongs to
+     * 
+     * @returns
+     *          string
+     */
+    getColor(): string {
+        return this.color;
+    }
+
+    /**
+     * Returns path of the image to go behind the skill
+     * 
+     * @returns
+     *          string
+     */
+    abstract getSkillBoxPath(): string;
+
+    
+    /**
      * Checks if the skill can have a poitn added or removed from it
      * 
      * @param modification
@@ -83,97 +110,8 @@ export class Skill {
      *              number of points allocated in a skill tree
      * 
      * @returns 
-     *         Boolean, whether the skills points can be modified or not
+     *         Boolean: whether the skills points can be modified or not
      */
-    validateModification(modification: number, allocatedSkillTreePoints: number): Boolean {
-
-        //Invalid number return false
-        if (modification < -1 || modification > 1 || modification == 0) return false;
-
-        //not enough pre-req points return false
-        if (allocatedSkillTreePoints < this.preReq) return false;
-        
-           //Action skill mod or 
-        if (this.getOtherSkillType() == this.ACTION_MOD_CASE || this.getOtherSkillType() == this.OTHER_CASE) {
-
-            //Check if this skill has a required action skill to be allocated
-            if (this.getRequiredActiveSkill() != null) {
-
-                //It does and it has no allocation, then return false
-                if (this.getRequiredActiveSkill().getAllocated() < 0) return false;
-            } 
-        }
-        
-
-        //At max points and the modification is addition return false
-        if (this.allocatedPoints == this.maxPoints && modification > 0) return false;
-
-        //At min points and the modification is subtraction return false
-        if (this.allocatedPoints == this.MIN_POINTS && modification < 0) return false;
-
-        //Modification successful
-        return true;
-    }
-
-    /**
-     * @returns
-     *          pre-req amount
-     */
-    getPreReq() {
-        return this.preReq;
-    }
-
-    /**
-     * @returns
-     *          points allocated into the skill
-     */
-    getAllocated() {
-        return this.allocatedPoints;
-    }
-
-    /**
-     * @returns
-     *          position of skill on tree
-     */
-    getPosition(): Array<number> {
-        return this.position;
-    }
-
-    /**
-     * @returns
-     *          path of image for skill
-     */
-    getPath(): string {
-        return this.path;
-    }
-
-    /**
-     * @returns
-     *          max allocated points
-     */
-    getMaxPoints(): number {
-        return this.maxPoints;
-    }
-
-    getSkillBoxPath(): string {
-        return this.skillBoxPath;
-    }
-
-    /**
-     * @returns
-     *          if the skill is active skill or active mod
-     */
-    getOtherSkillType(): number {
-        return this.otherSkillType;
-    }
-
-    /**
-     * @returns
-     *          required active skill to allocate mod
-     */
-    getRequiredActiveSkill(): Skill {
-        return this.requiredActiveSkill;
-    }
-
+    abstract validateModification(modification: number, allocatedSkillTreePoints: number): Boolean;
 
 }
