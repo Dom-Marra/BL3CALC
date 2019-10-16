c2db6a7b84b080841a25e9fddd7df7ed1f4c91cd
Dominic Marra <dominicmarra321@gmail.com>
2019-10-16 11:12:08 -0700

    Added simple scrollbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { SimplebarAngularModule } from 'simplebar-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { HomeComponent } from './home/home.component';
 import { SkilltreeComponent } from './skilltree/skilltree.component';
 import { StatsComponent } from './stats/stats.component';
 import { BuildComponent } from './build/build.component';
+import { TooltipComponent } from './tooltip/tooltip.component';
 
 @NgModule({
   declarations: [
@@ -21,13 +23,15 @@ import { BuildComponent } from './build/build.component';
     HomeComponent,
     SkilltreeComponent,
     StatsComponent,
-    BuildComponent
+    BuildComponent,
+    TooltipComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFontAwesomeModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SimplebarAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]
