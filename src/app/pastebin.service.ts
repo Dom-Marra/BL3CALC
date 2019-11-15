b7b8a470b139f886d6a370f17cce4cec783fb87d
Dominic Marra <dominicmarra321@gmail.com>
2019-11-15 14:03:32 -0800

    Modified pastebin to be https

--- src/app/pastebin.service.ts
+++ src/app/pastebin.service.ts
@@ -28,7 +28,7 @@ export class PastebinService {
   getBuild(saveLink: string) {
 
       var request = new XMLHttpRequest();
-      request.open("GET", "http://www.pastebin.com/raw/" + saveLink, true);
+      request.open("GET", "https://www.pastebin.com/raw/" + saveLink, true);
 
       request.send(saveLink);
     
@@ -67,7 +67,7 @@ export class PastebinService {
 
     var request = new XMLHttpRequest();
 
-    request.open("POST", "http://www.pastebin.com/api/api_post.php", true);
+    request.open("POST", "https://www.pastebin.com/api/api_post.php", true);
 
     request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
