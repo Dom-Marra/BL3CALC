57616a3c4e266fa028ad2f3c358d1ec6e8bd1e60
Dom-Marra <dominicmarra321@gmail.com>
2020-11-14 18:49:20 -0800

    Removed carousel functions

--- src/app/build/build.component.ts
+++ src/app/build/build.component.ts
@@ -66,51 +66,6 @@ export class BuildComponent implements OnInit {
   onHovered(emmitValues: Array<any>) {
     this.emmitValues = emmitValues;
   }
-  
-  /**
-   * Moves right carousel item to the current position, the
-   * left item to the next position, and current item to the previous position
-   */
-  next() {
-    var current = document.getElementsByClassName('active')[0];     //Current tree being presented
-    var next = document.getElementsByClassName('right')[0];         //prev tree to be presented
-    var prev = document.getElementsByClassName('left')[0];          //next tree presented
-
-    //current becomes prev
-    current.className = ' carousel-item left';
-    current.setAttribute('style', 'z-index: 1');
-
-    //next becomes current
-    next.className = ' carousel-item active';
-    next.setAttribute('style', 'z-index: 2');
-
-    //prev becomes next
-    prev.className = ' carousel-item right';
-    prev.setAttribute('style', 'z-index: 0');
-  }
-
-   /**
-   * Moves left carousel item to the current position, the
-   * right item to the previous position, and current item to the next position
-   */
-  previous() {
-    var current = document.getElementsByClassName('active')[0];     //Current tree being presented
-    var prev = document.getElementsByClassName('left')[0];          //prev tree to be presented
-    var next = document.getElementsByClassName('right')[0];         //next tree presented
-
-    //current becomes next
-    current.className = ' carousel-item right';
-    current.setAttribute('style', 'z-index: 1');
-
-    //prev becomes current
-    prev.className = ' carousel-item active';
-    prev.setAttribute('style', 'z-index: 2');
-
-    //next becomes prev
-    next.className = ' carousel-item left';
-    next.setAttribute('style', 'z-index: 0');
-
-  }
 
   /**
    * Creates the build data and sends it to the pastebin service for a link generation
