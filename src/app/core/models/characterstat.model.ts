95f6cded0ecdb3d08b98cbc5908855612ba4a51d
Dom-Marra <dominicmarra321@gmail.com>
2021-04-24 09:35:58 -0700

    Updated properties

--- src/app/core/models/characterstat.model.ts
+++ src/app/core/models/characterstat.model.ts
@@ -1,6 +1,7 @@
 export interface CharacterStat {
     text: string;                                  //text of the stats
-    value: number;                                 //Value of the stat
     valueType: 'flat' | 'percent';                 //value representation
     group: 'offense' | 'defense' | 'utility';      //group that the stat belongs to
+    value?: number;                                //Value of the stat
+    isMultiplier?: boolean;
 }
