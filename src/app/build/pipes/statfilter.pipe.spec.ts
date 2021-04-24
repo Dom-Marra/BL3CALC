a46bd0fb98fae32434af6a79b7991146d0fec06d
Dom-Marra <dominicmarra321@gmail.com>
2021-04-24 09:38:27 -0700

    Added pipe to filter by stat group

---
+++ src/app/build/pipes/statfilter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { StatfilterPipe } from './statfilter.pipe';
+
+describe('StatfilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new StatfilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/build/pipes/statfilter.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { CharacterStat } from 'src/app/core/models/characterstat.model';
+
+@Pipe({
+  name: 'statfilter',
+  pure: false
+})
+export class StatfilterPipe implements PipeTransform {
+
+  transform(stats: Map<string, CharacterStat>, group: string): Map<string, CharacterStat> {
+    let groupedStats: Map<string, CharacterStat> = new Map();
+
+    stats.forEach((val, key) => {
+      if (val.group == group && val.value && val.value > 0) groupedStats.set(key, val);  
+    });
+
+    return groupedStats;
+  }
+
+}
