955afc9aa0def2146ec1664ca50cb4f8947b4541
Dom-Marra <dominicmarra321@gmail.com>
2021-04-24 09:31:50 -0700

    Overhauled functionality

--- src/app/build/pages/stats/stats.component.ts
+++ src/app/build/pages/stats/stats.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from 'src/app/core/classes/character';
+import { Conditional } from 'src/app/core/models/conditional.model';
+import { CharacterService } from '../../services/character.service';
 
 @Component({
   selector: 'app-stats',
@@ -6,11 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stats.component.scss']
 })
 export class StatsComponent implements OnInit{
+
+  public character: Character;
   
-  constructor() { 
+  constructor(private characterService: CharacterService) { 
   }
 
   ngOnInit() {
+    this.characterService.currentCharacter.subscribe(character => {
+      if (character) this.character = character;
+    });
   }
 
+  public onConditionalChange(conditional: Conditional, conKey: string, isActive?: boolean, value?: number) {
+    let oldConditional: Conditional = {
+      isActive: conditional.isActive,
+      text: conditional.text,
+      value: conditional.value,
+      usesStacks: conditional.usesStacks,
+      stackKey: conditional.stackKey
+    }
+
+    if (isActive != null) conditional.isActive = isActive;
+    if (value != null) conditional.value = value;
+    
+    this.character.updateStatsBasedOnUpdatedConditional(oldConditional, conditional, conKey);
+  }
 }
