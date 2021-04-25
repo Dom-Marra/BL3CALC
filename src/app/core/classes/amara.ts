790227ac4c518066885d4ddf1e09a2c38e48a1f0
Dom-Marra <dominicmarra321@gmail.com>
2021-04-25 15:18:20 -0700

    Updated to work with new character functionality and character models

--- src/app/core/classes/amara.ts
+++ src/app/core/classes/amara.ts
@@ -3,29 +3,28 @@ import { OtherSkill } from './otherskill';
 import { ActionSkill } from './actionskill';
 import { ActionMod } from './actionmod';
 import { Character } from './character';
+import { CharacterModel } from '../models/character.model';
 import { SkillTree } from './skilltree';
-import { BLUE_SKILLS, GREEN_SKILLS, ORANGE_SKILLS, shockra } from '../data/amara/amaraskills';
-import { Conditional } from '../models/conditional.model';
-import { AMARA_CONDITIONALS } from '../data/amara/amaraconditionals';
-import { CharacterStat } from '../models/characterstat.model';
-import { AMARA_STATS } from '../data/amara/amarastats';
+import { BaseCharacterModel } from '../models/basecharacter.model';
 
 export class Amara extends Character {
 
-  private amaraConditionals: Map<string, Conditional> = AMARA_CONDITIONALS;
-  private amaraStats: Map<string, CharacterStat> = AMARA_STATS;
+  public readonly SHOCKRA: OtherSkill;
 
-  public greenTree: SkillTree = new SkillTree('green', 'assets/images/amara/GreenTreeHeader.png', 'Brawl', GREEN_SKILLS, this);
-  public blueTree: SkillTree = new SkillTree('blue', 'assets/images/amara/BlueTreeHeader.png', 'Mystic Assualt', BLUE_SKILLS, this);
-  public orangeTree: SkillTree = new SkillTree('orange', 'assets/images/amara/RedTreeHeader.png', 'Fist of The Elements', ORANGE_SKILLS, this);
+  constructor(baseCharacterData: BaseCharacterModel, characterData: CharacterModel) {
+    super(baseCharacterData, 1, 1, 1);
 
-  constructor(maxActionSkillPoints: number, maxActionModPoints: number, maxOtherSkillPoints: number) {
-    super(maxActionSkillPoints, maxActionModPoints, maxOtherSkillPoints);
     this.name = "Amara";
 
-    this.addPoint(shockra);
-    this.addConditionalMap(this.amaraConditionals);
-    this.addStatsMap(this.amaraStats);
+    this.greenTree = new SkillTree(characterData.trees.find(tree => tree.color == "green"), this);
+    this.orangeTree = new SkillTree(characterData.trees.find(tree => tree.color == "orange"), this);
+    this.blueTree = new SkillTree(characterData.trees.find(tree => tree.color == "blue"), this);
+
+    this.SHOCKRA = new OtherSkill(characterData.defaultSkills.find(x => x.name == "SHOCKRA"));
+
+    this.addConditionals(characterData.conditionals);
+    this.addStats(characterData.stats);
+    this.addPoint(this.SHOCKRA);
   }
 
   public handleAdditionOfNonNormalSkill(skill: Skill): void {
@@ -34,12 +33,12 @@ export class Amara extends Character {
 
       //Check to see if an action skill is allocated already
       //If there is remove a point from its allocation and remove it from equipped skills
-      if (this.getEquippedSkills()[0].actionSkill != skill && this.getEquippedSkills()[0].actionSkill != null)  {
-        this.removePoint(this.getEquippedSkills()[0].actionSkill);
+      if (this.equippedSkills[0].actionSkill != skill && this.equippedSkills[0].actionSkill != null)  {
+        this.removePoint(this.equippedSkills[0].actionSkill);
       } 
 
       //Add action skill to equipped skills
-      this.getEquippedSkills()[0].actionSkill = skill;
+      this.equippedSkills[0].actionSkill = skill;
     } 
 
     //Action mod
@@ -47,12 +46,12 @@ export class Amara extends Character {
       
       //Check to see if an action mod is allocated already
       //If there is remove a point from its allocation and remove it from equipped skills
-      if (this.getEquippedSkills()[0].actionMods[0] != skill && this.getEquippedSkills()[0].actionMods[0] != null)  {
-        this.removePoint(this.getEquippedSkills()[0].actionMods[0]);
+      if (this.equippedSkills[0].actionMods[0] != skill && this.equippedSkills[0].actionMods[0] != null)  {
+        this.removePoint(this.equippedSkills[0].actionMods[0]);
       } 
 
       //Add action mod to equipped skills action mod array
-      this.getEquippedSkills()[0].actionMods[0] = skill;
+      this.equippedSkills[0].actionMods[0] = skill;
     }
 
     //Other skill
@@ -60,30 +59,30 @@ export class Amara extends Character {
 
       //Check to see if an other skill is allocated already
       //If there is remove a point from its allocation and remove it from equipped skills
-      if (this.getEquippedSkills()[0].otherSkill != skill && this.getEquippedSkills()[0].otherSkill != null)  {
-        this.removePoint(this.getEquippedSkills()[0].otherSkill);
+      if (this.equippedSkills[0].otherSkill != skill && this.equippedSkills[0].otherSkill != null)  {
+        this.removePoint(this.equippedSkills[0].otherSkill);
       } 
       
       //Add other skill to equipped skills
-      this.getEquippedSkills()[0].otherSkill = skill;
+      this.equippedSkills[0].otherSkill = skill;
     }
   }
 
   public handleRemovalOfNonNormalSkill(skill: Skill, pos?: number): void {
     //remove action skill from equipped skills
     if (skill instanceof ActionSkill)  {
-      this.getEquippedSkills()[0].actionSkill = null;
+      this.equippedSkills[0].actionSkill = null;
     } 
 
     //Remove action mod from equipped skills action mod array
     if (skill instanceof ActionMod) {
-      this.getEquippedSkills()[0].actionMods[0] = null;
+      this.equippedSkills[0].actionMods[0] = null;
     }
 
     //Remove other skill from equipped skills
-    if (skill instanceof OtherSkill && skill != shockra) {
-      this.getEquippedSkills()[0].otherSkill = null;
-      this.addPoint(shockra);
+    if (skill instanceof OtherSkill && skill != this.SHOCKRA) {
+      this.equippedSkills[0].otherSkill = null;
+      this.addPoint(this.SHOCKRA);
     } 
   }
 }
