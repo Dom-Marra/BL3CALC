de9afb08c4524cab826007a95698e6a52e8ca0a0
Dom-Marra <dominicmarra321@gmail.com>
2021-05-03 14:40:58 -0700

    Exported scale text directive

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EdgeBoxComponent } from './components/edge-box/edge-box.component';
+import { ScaleTextDirective } from './directives/scale-text.directive';
 
 @NgModule({
   declarations: [
-    EdgeBoxComponent
+    EdgeBoxComponent,
+    ScaleTextDirective
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    EdgeBoxComponent
+    EdgeBoxComponent,
+    ScaleTextDirective
   ]
 })
 export class SharedModule { }
