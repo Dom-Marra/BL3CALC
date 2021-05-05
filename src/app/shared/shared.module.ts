f75bc8d9498144f1cdfbefc7f323f472e19dfa55
Dom-Marra <dominicmarra321@gmail.com>
2021-05-04 23:24:19 -0700

    Updated imports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { EdgeBoxComponent } from './components/edge-box/edge-box.component';
 import { ScaleTextDirective } from './directives/scale-text.directive';
+import { LoaderComponent } from './components/loader/loader.component';
+import { SnackbarComponent } from './components/snackbar/snackbar.component';
 
 @NgModule({
   declarations: [
     EdgeBoxComponent,
-    ScaleTextDirective
+    ScaleTextDirective,
+    LoaderComponent,
+    SnackbarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     EdgeBoxComponent,
-    ScaleTextDirective
+    ScaleTextDirective,
+    LoaderComponent,
+    SnackbarComponent
   ]
 })
 export class SharedModule { }
