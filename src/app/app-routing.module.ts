5586e0a12360e4cc5e15818372e3b9ef3ba21e99
Dom-Marra <dominicmarra321@gmail.com>
2021-09-30 11:23:35 -0700

    Added route for 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'build/amara', pathMatch: 'full'},
   {path: 'build', redirectTo: 'build/amara', pathMatch: 'full'},
   {path: 'build/:character', loadChildren: () => import('./build/build.module').then(m => m.BuildModule)},
   {path: 'build/:character/:build', loadChildren: () => import('./build/build.module').then(m => m.BuildModule)},
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
