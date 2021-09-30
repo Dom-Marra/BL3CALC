24b3c47a9c1ca1556c20148e1f0106e79c5f018e
Dom-Marra <dominicmarra321@gmail.com>
2021-09-29 18:52:59 -0700

    Updated link output per new URL

--- src/app/build/components/save-dialog/save-dialog.component.ts
+++ src/app/build/components/save-dialog/save-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { DocumentReference } from '@angular/fire/firestore';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Save } from 'src/app/core/models/save.model';
 @Component({
   selector: 'app-save-dialog',
   templateUrl: './save-dialog.component.html',
@@ -12,12 +13,12 @@ export class SaveDialogComponent implements OnInit {
   public message: string = '';
   public copied: boolean = false;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: Promise<DocumentReference<unknown>>, private dialogRef: MatDialogRef<SaveDialogComponent>) { 
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {firebase: Promise<DocumentReference<unknown>>, save: Save}, private dialogRef: MatDialogRef<SaveDialogComponent>) { 
     
     this.dialogRef.addPanelClass('save-dialog');
 
-    this.data.then(doc => {
-      this.message = 'https://bl3calc.com/build/skilltrees?build=' + doc.id;
+    this.data.firebase.then(doc => {
+      this.message = `https://bl3calc.com/build/${data.save.type.toLowerCase()}/${doc.id}`;
       this.status = 'success';
     }).catch(() => {
       this.message = "Sorry, there was an error during the save. Please try again."
