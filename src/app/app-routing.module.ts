f4aa0822c903d1e949399cffce19c8794205c0b5
Dom-Marra <dominicmarra321@gmail.com>
2021-09-29 17:59:51 -0700

    Url is more user friendly

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'build', pathMatch: 'full'},
-  {path: 'build', loadChildren: () => import('./build/build.module').then(m => m.BuildModule)}
+  {path: '', redirectTo: 'build/amara', pathMatch: 'full'},
+  {path: 'build', redirectTo: 'build/amara', pathMatch: 'full'},
+  {path: 'build/:character', loadChildren: () => import('./build/build.module').then(m => m.BuildModule)},
+  {path: 'build/:character/:build', loadChildren: () => import('./build/build.module').then(m => m.BuildModule)},
 ];
 
 @NgModule({
